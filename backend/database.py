9eebf2947eee73ba2194c37c4d5acddccb2ed3f2
Nicolás Vega <nicolasvegaguerrero10@gmail.com>
2023-09-19 19:23:56 -0300

    se crea el backend

--- backend/database.py
+++ backend/database.py
@@ -13,3 +13,27 @@ def get_task():
     cur.execute(sql)
     output = cur.fetchall()
     return output
+
+def add_note(text):
+    val = []
+    val.append((text))
+    cur = conn.cursor()
+    sql = "insert into notes (note, created_at) values (%s, now())"
+    cur.executemany(sql, val)
+    conn.commit()
+
+def edit_note(id_note, text):
+    val = []
+    val.append((text, id_note))
+    cur = conn.cursor()
+    sql = "update notes set note = %s where id_note = %s;"
+    cur.executemany(sql, val)
+    conn.commit()
+
+def delete_note(id_note):
+    val = []
+    val.append((id_note))
+    cur = conn.cursor()
+    sql = "delete from notes where id_note = %s"
+    cur.executemany(sql, val)
+    conn.commit()
--- backend/task.py
+++ backend/task.py
@@ -1,10 +1,29 @@
-import database
+import database as db
 from fastapi import APIRouter
 import database
+from pydantic import BaseModel
 
 app = APIRouter()
 
+class Notes(BaseModel):
+    text: str
+
+class NotesData(Notes):
+    id_note: int
+
 @app.get("/")
 async def root():
-    return database.get_task()
+    return db.get_task()
 
+@app.post("/add/")
+async def add(notes: Notes):
+    db.add_note(notes.text)
+
+@app.put("/editnote/")
+async def edit(note: NotesData):
+    db.edit_note(note.id_note, note.text)
+
+
+@app.delete("/delete/id={id_note}")
+async def delete(id_note: int):
+    db.delete_note(id_note)
